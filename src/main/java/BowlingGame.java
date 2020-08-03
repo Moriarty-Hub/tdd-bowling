73d8f0a9375844b93922ed3e21f711c46b42ee47
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-08-03 16:16:55 +0800

    Refactor the logic of calculate the total score of the 10th frame.

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -3,10 +3,7 @@ public class BowlingGame {
     private static final int BASE_SCORE_OF_STRIKE_OR_SPARE = 10;
 
     public int getFrameFinalScore(int[][] scorecard, int frame) {
-        if (frame == 10 && isSpare(scorecard[frame - 1])) {
-            return scorecard[frame - 1][0] + scorecard[frame - 1][1] + scorecard[frame - 1][2];
-        }
-        if (frame == 10 && isStrike(scorecard[frame - 1])) {
+        if (isSpareOrStrikeInTheTenthFrame(scorecard, frame)) {
             return scorecard[frame - 1][0] + scorecard[frame - 1][1] + scorecard[frame - 1][2];
         }
         if (isSpare(scorecard[frame - 1])) {
@@ -25,4 +22,9 @@ public class BowlingGame {
     private boolean isSpare(int[] scoreOfFrame) {
         return scoreOfFrame[0] != 10 && scoreOfFrame[0] + scoreOfFrame[1] == 10;
     }
+
+    private boolean isSpareOrStrikeInTheTenthFrame(int[][] scorecard, int frame) {
+        return frame == 10 && (isSpare(scorecard[frame - 1]) || isStrike(scorecard[frame - 1]));
+    }
+
 }
