a0220638d1db4ab0f2cc8edd6d95fb736ed6b3c9
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-08-03 15:43:17 +0800

    Return the sum of score of frame and next two throws when strike.

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,6 +1,9 @@
 public class BowlingGame {
 
     public int getFrameFinalScore(int[][] scorecard, int frame) {
+        if (scorecard[frame - 1][0] == 10) {
+            return 10 + scorecard[frame][0] + scorecard[frame][1];
+        }
         return scorecard[frame - 1][0] + scorecard[frame - 1][1];
     }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -11,4 +11,13 @@ public class BowlingGameTest {
         int score = bowlingGame.getFrameFinalScore(scorecard, 2);
         assertEquals(8, score);
     }
+
+    @Test
+    void should_return_the_score_sum_of_this_frame_and_next_two_throws_when_calculate_score_of_strike() {
+        BowlingGame bowlingGame = new BowlingGame();
+        int[][] scorecard = new int[][] {{3, 4}, {2, 6}, {1, 1}, {10, 0}, {2, 2}, {4, 6}, {5, 4}, {4, 1}, {1, 2}, {4, 2, 0}};
+        int score = bowlingGame.getFrameFinalScore(scorecard, 4);
+        assertEquals(14, score);
+    }
+
 }
