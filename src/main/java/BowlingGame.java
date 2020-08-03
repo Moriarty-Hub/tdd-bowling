1726c874cdf33e4a79030d0f83b60f09502a65f7
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-08-03 15:55:33 +0800

    Return the sum of score of three throws when got strike in the first throw of 10th frame.

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -3,6 +3,9 @@ public class BowlingGame {
     private static final int BASE_SCORE_OF_STRIKE_OR_SPARE = 10;
 
     public int getFrameFinalScore(int[][] scorecard, int frame) {
+        if (frame == 10 && isStrike(scorecard[frame - 1])) {
+            return scorecard[frame - 1][0] + scorecard[frame - 1][1] + scorecard[frame - 1][2];
+        }
         if (isSpare(scorecard[frame - 1])) {
             return BASE_SCORE_OF_STRIKE_OR_SPARE + scorecard[frame][0];
         }
