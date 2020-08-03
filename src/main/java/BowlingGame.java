74d893b504cf5e93f9a96ff27de9454237979a41
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-08-03 15:49:39 +0800

    Refactor BowlingGame, add two private method for judging if the frame is strike or spare.

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,12 +1,22 @@
 public class BowlingGame {
 
+    private static final int BASE_SCORE_OF_STRIKE_OR_SPARE = 10;
+
     public int getFrameFinalScore(int[][] scorecard, int frame) {
-        if (scorecard[frame - 1][0] != 10 && scorecard[frame - 1][0] + scorecard[frame - 1][1] == 10) {
-            return 10 + scorecard[frame][0];
+        if (isSpare(scorecard[frame - 1])) {
+            return BASE_SCORE_OF_STRIKE_OR_SPARE + scorecard[frame][0];
         }
-        if (scorecard[frame - 1][0] == 10) {
-            return 10 + scorecard[frame][0] + scorecard[frame][1];
+        if (isStrike(scorecard[frame - 1])) {
+            return BASE_SCORE_OF_STRIKE_OR_SPARE + scorecard[frame][0] + scorecard[frame][1];
         }
         return scorecard[frame - 1][0] + scorecard[frame - 1][1];
+    }
+
+    private boolean isStrike(int[] scoreOfFrame) {
+        return scoreOfFrame[0] == 10;
+    }
+
+    private boolean isSpare(int[] scoreOfFrame) {
+        return scoreOfFrame[0] != 10 && scoreOfFrame[0] + scoreOfFrame[1] == 10;
     }
 }
